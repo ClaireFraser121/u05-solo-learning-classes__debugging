fa46220b1ab913bf2afad2d6a4f546f79e142c0e
Claire Fraser <145777291+ClaireFraser121@users.noreply.github.com>
2025-10-17 22:43:13 +0100

    add diary multi-class demo all tests passing

---
+++ tests/test_unit_diary_entry.py
@@ -0,0 +1,82 @@
+from lib.diary_entry import DiaryEntry
+
+"""
+When I initialise with a tile and contents
+I can get that title and contents back
+"""
+
+def test_constructs_and_gets_title_and_contents():
+    diary_entry = DiaryEntry("My Title", "My Contents")
+    assert diary_entry.title == "My Title"
+    assert diary_entry.contents == "My Contents"
+
+
+"""
+When I initialise with a five-word contents 
+Then #count_words should return five
+"""
+def test_count_words_returns_word_count_of_contents():
+    diary_entry = DiaryEntry("My Title", "one two theww four five")
+    assert diary_entry.count_words() == 5
+
+"""
+When I initilise with a five-word contents 
+then #read_time with a wpm of 2 should return 3
+"""
+
+def test_reading_time():
+    diary_entry = DiaryEntry("My Title", "one two theww four five")
+    assert diary_entry.reading_time(2) == 3
+
+"""
+When I initialise with a five-word contents
+Then, at first, #reading_chunk should return the first chunk
+    readable in the time
+"""
+def test_readable_chunk_first_chunk():
+    diary_entry = DiaryEntry("My Title", "one two three four five")
+    assert diary_entry.reading_chunk(2, 1) == "one two"
+
+"""
+When I initialise with a five-word contents
+Then, on the second call, #reading_chuck should return the second chunk
+    readable in the time
+"""
+
+def test_readable_chunk_second_chunk():
+    diary_entry = DiaryEntry("My Title", "one two three four five")
+    diary_entry.reading_chunk(2, 1) 
+    assert diary_entry.reading_chunk(2, 1) == "three four"
+
+"""
+When I initialise with a five-word contents
+Then, on the third call, #reading_chuck should return the final partial chunk
+"""
+
+def test_readable_chunk_third_chunk():
+    diary_entry = DiaryEntry("My Title", "one two three four five")
+    diary_entry.reading_chunk(2, 1)
+    diary_entry.reading_chunk(2, 1)
+    assert diary_entry.reading_chunk(2, 1) == "five"
+
+"""
+When I initialise Iwith a five-words contents
+Then, on the fouth call, #reading_chunk should start again from the beginning
+"""
+def test_readable_chunk_fourth_chunk():
+    diary_entry = DiaryEntry("My Title", "one two three four five")
+    diary_entry.reading_chunk(2, 1)
+    diary_entry.reading_chunk(2, 1)
+    diary_entry.reading_chunk(2, 1)
+    assert diary_entry.reading_chunk(2, 1) == "one two"
+
+"""
+When I initialise Iwith a five-words contents
+Then, on the fouth call, #reading_chunk should start again from the beginning
+"""
+def test_readable_chunk_fourth_chunk_with_exact_chunks():
+    diary_entry = DiaryEntry("My Title", "one two three four five six")
+    diary_entry.reading_chunk(2, 1)
+    diary_entry.reading_chunk(2, 1)
+    diary_entry.reading_chunk(2, 1)
+    assert diary_entry.reading_chunk(2, 1) == "one two"
